0c631c31ddbfb14dcdb4536d316722a16d20f2e0
Oleh Yablunovskyi <cowperwood777@gmail.com>
2023-03-30 19:40:38 +0300

    Add CommentForm component

---
+++ frontend/src/components/CommentForm/CommentForm.tsx
@@ -0,0 +1,145 @@
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { FormDataType } from '../../types/FormDataType';
+import './CommentForm.scss';
+
+const initialFormData: FormDataType = {
+  userName: '',
+  email: '',
+  homePage: '',
+  message: '',
+  imageFile: null,
+  textFile: null,
+};
+
+export const CommentForm = () => {
+  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState<FormDataType>(initialFormData);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { id, value } = event.target;
+
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
+
+  const resetFormData = () => {
+    setFormData(initialFormData);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(formData);
+
+    const payload = new FormData();
+
+    Object.entries(formData).forEach(([key, value]) => {
+      if (value !== null) {
+        payload.append(key, value);
+      }
+    });
+
+    payload.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+    });
+
+    setCount((prevCount => prevCount + 1));
+    resetFormData();
+  };
+
+  return (
+    <form className="Form" onSubmit={handleSubmit} key={count}>
+      <h2 className="Form__title">Add Your Comment</h2>
+      <div className="Form__group">
+        <label htmlFor="username" className="Form__label">
+          <p className="Form__caption">User Name (required):</p>
+
+          <input
+            type="text"
+            id="userName"
+            required
+            pattern="[A-Za-z0-9 ]+"
+            className="Form__input"
+            onChange={handleInputChange}
+          />
+        </label>
+      </div>
+
+      <div className="Form__group">
+        <label htmlFor="email" className="Form__label">
+          <p className="Form__caption">E-mail (required):</p>
+
+          <input
+            type="email"
+            id="email"
+            required
+            className="Form__input"
+            onChange={handleInputChange}
+          />
+        </label>
+
+      </div>
+
+      <div className="Form__group">
+        <label htmlFor="homepage" className="Form__label">
+          <p className="Form__caption">Home page (optional):</p>
+
+          <input
+            type="url"
+            id="homePage"
+            className="Form__input"
+            onChange={handleInputChange}
+          />
+        </label>
+      </div>
+
+      <div className="Form__group">
+        <label htmlFor="message" className="Form__label">
+          <p className="Form__caption">Text (required):</p>
+        </label>
+
+        <div className="Form__messageBlock">
+          <textarea
+            id="message"
+            required
+            className="Form__textarea"
+            onChange={handleInputChange}
+          >
+          </textarea>
+        </div>
+      </div>
+
+      <div className="Form__group">
+        <label htmlFor="imageFile" className="Form__label">
+          <p className="Form__caption">
+            Upload Image (JPG, GIF, PNG, max 320x240px):
+          </p>
+
+          <input
+            type="file"
+            id="imageFile"
+            accept="image/jpeg,image/gif,image/png"
+            className="Form__input Form__inputFile"
+          />
+        </label>
+      </div>
+
+      <div className="Form__group">
+        <label htmlFor="textFile" className="Form__label">
+          <p className="Form__caption">
+            Upload Text File (TXT, max 100KB):
+          </p>
+
+          <input
+            type="file"
+            id="textFile"
+            accept=".txt"
+            className="Form__input Form__inputFile"
+          />
+        </label>
+      </div>
+
+      <button type="submit" className="Form__submitButton">Submit</button>
+    </form>
+  );
+};
