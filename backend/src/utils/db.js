1513f28e8b690ad8c27fc93af1ca5f0ab98ad33e
Oleh Yablunovskyi <cowperwood777@gmail.com>
2023-04-04 12:20:06 +0300

    Set up User and Comment models and their associations

---
+++ backend/src/utils/db.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+'use strict';
+
+const { Sequelize } = require('sequelize');
+
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL is missing from the environment variables');
+}
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+});
+
+module.exports = sequelize;
